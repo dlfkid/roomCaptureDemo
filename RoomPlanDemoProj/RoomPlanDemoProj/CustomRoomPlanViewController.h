b2c7d29b0af36cd342985697a6866c745d0faaf5
ravendeng <ravendeng@tencent.com>
2022-11-02 11:54:02 +0800

    feat: create pod dependency & ui build

---
+++ RoomPlanDemoProj/RoomPlanDemoProj/CustomRoomPlanViewController.h
@@ -0,0 +1,16 @@
+//
+//  CustomRoomPlanViewController.h
+//  RoomPlanDemoProj
+//
+//  Created by ravendeng on 2022/11/2.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomRoomPlanViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- RoomPlanDemoProj/RoomPlanDemoProj/ViewController.m
+++ RoomPlanDemoProj/RoomPlanDemoProj/ViewController.m
@@ -6,8 +6,19 @@
 //
 
 #import "ViewController.h"
+#import <Masonry/Masonry.h>
+#import "BasicRoomPlanViewController.h"
+#import "CustomRoomPlanViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UILabel *titleLabel;
+
+@property (nonatomic, strong) UITextView *descriptionTextView;
+
+@property (nonatomic, strong) UIButton *baseicRPButton;
+
+@property (nonatomic, strong) UIButton *customRPButton;
 
 @end
 
@@ -15,8 +26,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.titleLabel = [[UILabel alloc] init];
+    self.titleLabel.text = @"RoomPlanDemo";
+    self.titleLabel.font = [UIFont boldSystemFontOfSize:36];
+    [self.view addSubview:self.titleLabel];
+    
+    self.descriptionTextView = [[UITextView alloc] initWithFrame:CGRectZero];
+    self.descriptionTextView.userInteractionEnabled = NO;
+    self.descriptionTextView.text = [self roomPlanDescriptionText];
+    [self.descriptionTextView sizeToFit];
+    [self.view addSubview:self.descriptionTextView];
+    
+    self.baseicRPButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.baseicRPButton setTitle:@"BasicRoomPlanUsage" forState:UIControlStateNormal];
+    [self.view addSubview:self.baseicRPButton];
+    
+    self.customRPButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.customRPButton setTitle:@"customRoomPlanUsage" forState:UIControlStateNormal];
+    [self.view addSubview:self.customRPButton];
+    
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.view.mas_centerX);
+        make.top.mas_equalTo(self.view.mas_safeAreaLayoutGuideTop).offset(30);
+    }];
+    
+    [self.descriptionTextView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.titleLabel.mas_bottom).offset(30);
+        make.left.mas_equalTo(16);
+        make.right.mas_equalTo(-16);
+        make.height.mas_greaterThanOrEqualTo(100);
+    }];
+    
+    [self.baseicRPButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.descriptionTextView.mas_centerX);
+        make.top.mas_equalTo(self.descriptionTextView.mas_bottom).mas_offset(30);
+    }];
+    
+    [self.customRPButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.baseicRPButton.mas_centerX);
+        make.top.mas_equalTo(self.baseicRPButton.mas_bottom).mas_offset(30);
+    }];
 }
 
+- (NSString *)roomPlanDescriptionText {
+    return @"To scan your room, point your device at all the walls, windows, doors and furniture in your space until your scan is complete. \n You can see a preview of your scan at the bottom of the screen so you can make sure your scan is correct. ";
+}
 
 @end
