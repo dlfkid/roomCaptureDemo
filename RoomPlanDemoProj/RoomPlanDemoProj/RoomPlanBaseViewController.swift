ee0cc10fd78af06c0adba4815ce1fb75868668c2
ravendeng <dlfkid@icloud.com>
2022-11-03 20:51:03 +0800

    chore: UI completion

--- RoomPlanDemoProj/RoomPlanDemoProj/RoomPlanBaseViewController.swift
+++ RoomPlanDemoProj/RoomPlanDemoProj/RoomPlanBaseViewController.swift
@@ -6,22 +6,44 @@
 //
 
 import UIKit
+import RoomPlan
+import PinLayout
 
 class RoomPlanBaseViewController: UIViewController {
+    
+    lazy private var roomCaptureView: RoomCaptureView = {
+        let view = RoomCaptureView(frame: self.view.bounds)
+        view.captureSession.delegate = self
+        view.delegate = self
+        return view
+    }()
 
-    lazy private var closeButton:UIButton = {
+    lazy private var closeButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("close", for: .normal)
         button.addTarget(self, action: #selector(closeButtonDidTappedAction), for: .touchUpInside)
         return button
     }()
     
-    lazy private var doneButton:UIButton = {
+    lazy private var doneButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("done", for: .normal)
         button.addTarget(self, action: #selector(doneButtonDidTappedAction), for: .touchUpInside)
         return button
     }()
+    
+    lazy private var exportButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.setTitle("Export", for: .normal)
+        button.layer.cornerRadius = 22
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+    
+    private var isScanning: Bool = false
+    
+    private var finishedCapturedRoom: CapturedRoom?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +51,9 @@ class RoomPlanBaseViewController: UIViewController {
         view.backgroundColor = .white
         view.addSubview(closeButton)
         view.addSubview(doneButton)
+        view.addSubview(exportButton)
+        
+        exportButton.isEnabled = false
     }
     
     override func viewDidLayoutSubviews() {
@@ -37,6 +62,8 @@ class RoomPlanBaseViewController: UIViewController {
         closeButton.pin.top(view.safeAreaInsets.top).marginTop(20).left(16).sizeToFit()
         
         doneButton.pin.top(to: closeButton.edge.top).right(16).sizeToFit()
+        
+        exportButton.pin.width(50%).height(44).hCenter().bottom(view.safeAreaInsets.bottom).marginBottom(20)
     }
     
     @objc func closeButtonDidTappedAction() {
@@ -44,11 +71,49 @@ class RoomPlanBaseViewController: UIViewController {
     }
     
     @objc func doneButtonDidTappedAction() {
-        
+        if isScanning {
+            stopSession()
+        }
+        closeButtonDidTappedAction()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startSession()
+    }
+    
+    override func viewWillDisappear(_ flag: Bool) {
+        super.viewWillDisappear(flag)
+        stopSession()
+    }
+    
+    private func startSession() {
+        isScanning = true
+        let configuration = RoomCaptureSession.Configuration()
+        roomCaptureView.captureSession.run(configuration: configuration)
+    }
+    
+    private func stopSession() {
+        isScanning = false
+        roomCaptureView.captureSession.stop()
+        exportButton.isEnabled = true
     }
 
 }
 
-extension RoomPlanBaseViewController {
+extension RoomPlanBaseViewController: RoomCaptureSessionDelegate {
     
+}
+
+extension RoomPlanBaseViewController: RoomCaptureViewDelegate {
+    func captureView(shouldPresent roomDataForProcessing: CapturedRoomData, error: Error?) -> Bool {
+        guard error == nil else {
+            return false
+        }
+        return true
+    }
+    
+    func captureView(didPresent processedResult: CapturedRoom, error: Error?) {
+        finishedCapturedRoom = processedResult
+    }
 }
--- RoomPlanDemoProj/RoomPlanDemoProj/RoomPlanCustomViewController.swift
+++ RoomPlanDemoProj/RoomPlanDemoProj/RoomPlanCustomViewController.swift
@@ -10,7 +10,26 @@ import PinLayout
 
 class RoomPlanCustomViewController: UIViewController {
     
+    lazy private var closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("close", for: .normal)
+        button.addTarget(self, action: #selector(closeButtonDidTappedAction), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
-        
+        super.viewDidLoad()
+        self.title = "Custom Room Plan"
+        view.backgroundColor = .white
+        view.addSubview(closeButton)
+    }
+    
+    @objc func closeButtonDidTappedAction() {
+        self.dismiss(animated: true)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        closeButton.pin.top(view.safeAreaInsets.top).marginTop(20).left(16).sizeToFit()
     }
 }
